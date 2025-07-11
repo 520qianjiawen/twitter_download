efaf742679c635e45177376b614f65d71bd56627
520qianjiawen <520qianjiawen@sina.com>
2025-07-11 14:57:04 +0800

    X

--- src/app/components/ui/FriendsLink.js
+++ src/app/components/ui/FriendsLink.js
@@ -13,22 +13,22 @@ import { getTranslation } from "@/lib/i18n";
 const friendsLink = [
     {
       name: "Travel Map Video",
-      url: "https://travelmap.video",
+      url: "#",
       icon: "https://travelmap.video/images/logo.png"
     },
     {
         name: "PDF Tool My",
-        url: "https://pdftoolmy.com",
+        url: "#",
         icon: "https://pdftoolmy.com/favicon.svg"
     },
     {
         name: "Temp Mail My",
-        url: "https://tempmailmy.com",
+        url: "#",
         icon: "https://tempmailmy.com/logo.png"
     },
     {
         name: "Cloud Desktop",
-        url: "https://desktop.dashu.ai",
+        url: "#",
         icon: "https://www.thiings.co/_next/image?url=https%3A%2F%2Flftz25oez4aqbxpq.public.blob.vercel-storage.com%2Fimage-V5tMC6Q2PJiLsi6mf9ZfAzI6aJ0aZm.png&w=3840&q=75"
     }
 ]
